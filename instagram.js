27eddca815731d86c6741a6ab4b67a04e68e534e
Ruth <emailtoruth@virginmedia.com>
2015-09-23 16:21:36 +0100

    initial TDD

--- instagram.js
+++ instagram.js
@@ -1,1 +1,16 @@
+// TESTING
+  test('Has the search button been clicked', function(assert) {
+      var sumbitButton = document.getElementById("searchBtn");
+      var clicked = false;
+      sumbitButton.addEventListener("click", function(){clicked = true;})
 
+      assert.equal(clicked,true,'message')
+
+  });
+
+
+
+// document.getElementById("searchBtn").addEventListener("click", search);
+// function search() {
+//     location.href = "http://liberty-x.github.io/lxnews/results";
+// }
