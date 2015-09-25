118ef8ce704c3f84d1be2879fdc8db8e6eddcd0c
Ruth <emailtoruth@virginmedia.com>
2015-09-25 14:40:16 +0100

    minor css changes

--- guardian.js
+++ guardian.js
@@ -70,7 +70,7 @@ function displayResultsFunction(arr) {
     for (index = oldGuard.length - 1; index >= 0; index--) {
       oldGuard[index].parentNode.removeChild(oldGuard[index]);
     }
- for(var i = 0 ; i < 9 ; i++){
+ for(var i = 0 ; i < 5 ; i++){
    var articleDiv = document.createElement("div");
     articleDiv.setAttribute('id', 'articleDest' + [i]);
     articleDiv.setAttribute('class', 'guardArticle');
