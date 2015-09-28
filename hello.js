4a53a09c424aafca1f4004899320fc5a8bce399d
Huw <hdrdavies@gmail.com>
2015-09-28 17:15:12 +0100

    removing node_module files

--- hello.js
+++ hello.js
@@ -4,3 +4,4 @@ http.createServer(function (req, res) {
   res.end('Hello Travis!\n') // this will FAIL travis ci lint
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
+console.log('fun times');
