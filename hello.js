11bc7cfba8dcbc150afd3699990ba6af9788237c
Huw Davies <hdrdavies@gmail.com>
2015-09-28 17:41:10 +0100

    typos

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n') // this will FAIL travis ci lint
+  res.end('Hello Travis!\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 console.log('fun times');
